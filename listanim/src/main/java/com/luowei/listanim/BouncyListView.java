d13ecd8eb2509c983ed9bdbec2ff4ead3a53497f
luowei <wei.luo@karakal.com.cn>
2016-06-27 10:41:09 +0800

    update

---
+++ listanim/src/main/java/com/luowei/listanim/BouncyListView.java
@@ -0,0 +1,165 @@
+package com.luowei.listanim;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.animation.TranslateAnimation;
+import android.widget.ListView;
+
+public class BouncyListView extends ListView {
+    private Context context;
+    private boolean outBound = false;
+    private int distance;
+    private int firstOut;
+    private static final String TAG = "wljie";
+
+    public BouncyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        Log.d(TAG, "IN 1");
+    }
+
+    public BouncyListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+        Log.d(TAG, "IN 2");
+    }
+
+    public BouncyListView(Context context) {
+        super(context);
+        this.context = context;
+        Log.d(TAG, "IN 3");
+    }
+
+    @SuppressWarnings("deprecation")
+    GestureDetector gestureDetector = new GestureDetector(
+            new OnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return false;
+                }
+
+                @Override
+                public void onShowPress(MotionEvent e) {
+                }
+
+                /**
+                 * 手势滑动的时候触发
+                 */
+                @Override
+                public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                        float distanceX, float distanceY) {
+                    Log.d(TAG, "ENTER onscroll");
+                    int firstPos = getFirstVisiblePosition();
+                    int lastPos = getLastVisiblePosition();
+                    int itemCount = getCount();
+                    // outbound Top
+                    if (outBound && firstPos != 0 && lastPos != (itemCount - 1)) {
+                        scrollTo(0, 0);
+                        return false;
+                    }
+                    View firstView = getChildAt(firstPos);
+                    if (!outBound)
+                        firstOut = (int) e2.getRawY();
+                    if (firstView != null
+                            && (outBound || (firstPos == 0
+                            && firstView.getTop() == 0 && distanceY < 0))) {
+                        // Record the length of each slide
+                        distance = firstOut - (int) e2.getRawY();
+                        scrollTo(0, distance / 2);
+                        return true;
+                    }
+                    // outbound Bottom
+                    return false;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    // TODO Auto-generated method stub
+                }
+
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                       float velocityX, float velocityY) {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                @Override
+                public boolean onDown(MotionEvent e) {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+            });
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.d(TAG, "dispatchTouchEvent");
+        int act = event.getAction();
+        if ((act == MotionEvent.ACTION_UP || act == MotionEvent.ACTION_CANCEL)
+                && outBound) {
+            outBound = false;
+            // scroll back
+        }
+        if (!gestureDetector.onTouchEvent(event)) {
+            outBound = false;
+        } else {
+            outBound = true;
+        }
+        Rect rect = new Rect();
+        getLocalVisibleRect(rect);
+        TranslateAnimation am = new TranslateAnimation(0, 0, -rect.top, 0);
+        am.setDuration(1000);
+        startAnimation(am);
+        scrollTo(0, 0);
+        return super.dispatchTouchEvent(event);
+    }
+
+
+/*
+    //实现过度滑动，item跟随手势的代码是这一段：  Java代码
+    int firstPos = getFirstVisiblePosition();
+    int lastPos = getLastVisiblePosition();
+    int itemCount = getCount();
+if(outBound&&firstPos!=0&&lastPos!=(itemCount-1))
+
+        {
+        scrollTo(0,0);
+        return false;
+        }
+
+        View firstView=getChildAt(firstPos);
+        if(!outBound)firstOut=(int)e2.getRawY();
+        if(firstView!=null&&(outBound||(firstPos==0&&firstView.getTop()==0&&distanceY<
+        0)))
+
+{
+        distance=firstOut-(int)e2.getRawY();
+        scrollTo(0,distance/2);
+        return true;
+        }
+
+        scrollTo(0,distance/2);
+
+        使得item只是过度滑动你所滑动距离的一半，这样更美观。而实现弹性滑动的基本思路就是滑动回去加个时间。代码如下：
+        Java代码 Rect
+        rect=new
+
+        Rect();
+
+        getLocalVisibleRect(rect);
+
+        TranslateAnimation am=new TranslateAnimation(0,0,-rect.top,0);
+        am.setDuration(300);
+
+        startAnimation(am);
+
+        scrollTo(0,0);
+        }}
+        */
+}
---
+++ listanim/src/main/java/com/luowei/listanim/MainActivity.java
@@ -0,0 +1,68 @@
+package com.luowei.listanim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+    private List<String> data = new ArrayList<>();
+    private LayoutInflater inflater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inflater = getLayoutInflater();
+        for (int i = 0; i < 50; i++) {
+            data.add("luowei_" + i);
+        }
+        lv = (ListView) findViewById(R.id.lv);
+        lv.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return data.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return data.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = inflater.inflate(R.layout.list_item_main, parent, false);
+                }
+                TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+                tvName.setText(data.get(position));
+                return convertView;
+            }
+        });
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+    }
+}
